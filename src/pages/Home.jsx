8fe0b30bc12ff67abcb38f67834e7dd82d6464e2
Samer taha <alrazy1@gmail.com>
2023-11-15 16:19:05 +0200

    added needed assets and 3d models to the project also created the island 3d component from sketchfab and gltf.pmnd.rs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import Loader from '../components/Loader'
+import Island from '../models/Island'
 
 {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
   POPUP
@@ -7,8 +10,20 @@ import React from 'react'
 const Home = () => {
   return (
     <section className='w-full h-screen relative'>
+      <Canvas
+        className='w-full h-screen bg-transparent'
+        camera={{ near: 0.1, far: 100 }}
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight />
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
 
-      
+          <Island />
+        </Suspense>
+      </Canvas>
     </section>
   )
 }
